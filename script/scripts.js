b23ee9ad9dee6d6237cb2915215ed9e4920b2dc0
wiesingerjonas <j.wiesinger@students.htl-leonding.ac.at>
2021-05-19 21:25:55 +0200

    fix bug

--- script/scripts.js
+++ script/scripts.js
@@ -1,8 +1,10 @@
+let active = 0;
+
 window.addEventListener('load', () => {
   const arrowLeft = document.getElementById('arrowLeft');
   const arrowRight = document.getElementById('arrowRight');
   const imageBox = document.getElementById('overflowWrapper');
-  const howmany = 4;
+  const howmany = 28;
   const images = [];
   const src = [];
 
@@ -10,17 +12,7 @@ window.addEventListener('load', () => {
     src[i] = `img/tf${i + 1}.jpg`;
   }
 
-  for (let i = 0; i < src.length; i++) {
-    images[i] = document.createElement('img');
-    images[i].src = src[i];
-    images[i].alt = i + 1 + '. Bild';
-
-    imageBox.appendChild(images[i]);
-    images[i].style.display = 'none';
-  }
-
-  images[0].style.display = 'block';
-  images[0].classList.add('imgAnimation');
+  refresh();
 
   document.getElementById('iconWrapper').style.height = document.images[0].clientHeight + 'px';
 
@@ -28,43 +20,25 @@ window.addEventListener('load', () => {
   arrowRight.addEventListener('click', goRight);
 
   function goRight() {
-    let rack = images[images.length - 1].src;
-    images[0].style.opacity = 0;
-
-    setTimeout(() => {
-      for (let i = images.length - 1; i > 0; i--) {
-        images[i].src = images[i - 1].src;
-      }
-
-      images[0].classList.remove('imgAnimation');
-      images[0].src = rack;
-      images[0].style.opacity = 1;
-
-      setTimeout(() => {
-        images[0].classList.add('imgAnimation');
-      }, 100);
-    }, 0);
-
-    goRightInterval = setInterval(() => {
-    }, 9500);
+    if (active === howmany-1) {
+      active = 0;
+    } else {
+      active++;
+    }
+    refresh();
   }
 
   function goLeft() {
-    clearInterval(goRightInterval);
-    let rack = images[0].src;
-    images[0].style.opacity = 0;
+    if (active === 0){
+      active = howmany-1
+    } else {
+      active--;
+    }
 
-    setTimeout(() => {
-      for (let i = 0; i < images.length - 1; i++) {
-        images[i].src = images[i + 1].src;
-      }
+    refresh();
+  }
 
-      images[images.length - 1].src = rack;
-      images[0].style.opacity = 1;
-    }, 250);
-
-    goRightInterval = setInterval(() => {
-      goRight();
-    }, 9500);
+  function refresh() {
+    document.getElementById('image').src = src[active];
   }
 });
